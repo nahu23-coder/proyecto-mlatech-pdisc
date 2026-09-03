e67f3ae658583b0485aebbaed94d687704d2001c
tz <tizianomaric@gmail.com>
2026-09-03 20:28:41 -0300

    modificacion del login

--- src/controllers/auth/login.php
+++ src/controllers/auth/login.php
@@ -1,2 +1,12 @@
 <?php
-# Implementar...
+if ($_SERVER['REQUEST_METHOD'] !== 'POST') {  // Si la solicitud no es POST, volves al register
+  header('Location: /src/views/auth/login.php');
+  exit;
+}
+
+$data = [
+  'email'           => trim($_POST['email'] ?? ''),
+  'password'        => $_POST['password'] ?? '',
+
+];
+?>
